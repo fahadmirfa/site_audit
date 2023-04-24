2db2a346dc8c7c612d18543dc8647b6a14628ce7
fahadmirfa <mirzafahad14150@gmail.com>
2023-04-24 22:23:49 +0500

    This is my first comiit of site_audit app

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Site Audit Apliction',
       debugShowCheckedModeBanner: false,
       theme: TAppTheme.lightTheme,
       themeMode: ThemeMode.system,
